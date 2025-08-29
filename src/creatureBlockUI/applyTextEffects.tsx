65a2d6482921f5d12cf0daa4b58806657bab151b
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2025-08-29 15:58:40 -0700

    Remove markdown bolding stars from text

--- src/creatureBlockUI/applyTextEffects.tsx
+++ src/creatureBlockUI/applyTextEffects.tsx
@@ -3,24 +3,27 @@ export function applyTextEffects(string: string) {
   const characteristicTestRegex =
     /((?:Might|Agility|Intuition|Reason|Presence)(?: test))+/g;
 
-  return string.split(potencyRegex).map((val, index) =>
-    index % 2 ? (
-      <span
-        key={index}
-        className="bg-zinc-950 rounded-sm font-semibold px-0.5 text-xs text-white"
-      >
-        {val}
-      </span>
-    ) : (
-      val.split(characteristicTestRegex).map((val, index) =>
-        index % 2 ? (
-          <span key={index} className="font-semibold">
-            {val}
-          </span>
-        ) : (
-          <span key={index}>{val}</span>
+  return string
+    .replaceAll("*", "")
+    .split(potencyRegex)
+    .map((val, index) =>
+      index % 2 ? (
+        <span
+          key={index}
+          className="bg-zinc-950 rounded-sm font-semibold whitespace-nowrap px-0.5  text-xs text-white"
+        >
+          {val}
+        </span>
+      ) : (
+        val.split(characteristicTestRegex).map((val, index) =>
+          index % 2 ? (
+            <span key={index} className="font-semibold">
+              {val}
+            </span>
+          ) : (
+            <span key={index}>{val}</span>
+          )
         )
       )
-    )
-  );
+    );
 }
